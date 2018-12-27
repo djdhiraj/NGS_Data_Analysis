f675cd3be942ed210f660d25470c10b225378422
Dhiraj <dhiraj.jamia@gmail.com>
2018-12-27 13:46:32 +0530

    Create Data_set1.R

---
+++ Data_set1.R
@@ -0,0 +1,26 @@
+getncbiseq <- function(accession)
+{
+  require("seqinr") # this function requires the SeqinR R package
+  # first find which ACNUC database the accession is stored in:
+  dbs <- c("genbank","refseq","refseqViruses","bacterial")
+  numdbs <- length(dbs)
+  for (i in 1:numdbs)
+  {
+    db <- dbs[i]
+    choosebank(db)
+    # check if the sequence is in ACNUC database 'db':
+    resquery <- try(query(".tmpquery", paste("AC=", accession)), silent = TRUE)
+    if (!(inherits(resquery, "try-error")))
+    {
+      queryname <- "query2"
+      thequery <- paste("AC=",accession,sep="")
+      query(`queryname`,`thequery`)
+      # see if a sequence was retrieved:
+      seq <- getSequence(query2$req[[1]])
+      closebank()
+      return(seq)
+    }
+    closebank()
+  }
+  print(paste("ERROR: accession",accession,"was not found"))
+}
