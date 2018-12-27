6d733643e488b29154ecf67a8a20018dd4f6d0fc
Dhiraj <dhiraj.jamia@gmail.com>
2018-12-27 15:18:44 +0530

    Update Demo.R

--- Demo.R
+++ Demo.R
@@ -53,3 +53,28 @@ chunk <- dengueseq[starts[i]:(starts[i]+1999)]
 chunkGC <- GC(chunk)
 print (chunkGC)
 }
+for (i in 1:n) {
+  chunk <- dengueseq[starts[i]:(starts[i]+1999)]
+  chunkGC <- GC(chunk)
+  print(chunkGC)
+  chunkGCs[i] <- chunkGC
+}
+
+slidingwindowplot <- function(windowsize, inputseq)
+{
+  starts <- seq(1, length(inputseq)-windowsize, by = windowsize)
+  n <- length(starts) # Find the length of the vector "starts"
+  chunkGCs <- numeric(n) # Make a vector of the same length as vector "starts",
+  #˓→but just containing zeroes
+  for (i in 1:n) {
+    chunk <- inputseq[starts[i]:(starts[i]+windowsize-1)]
+    chunkGC <- GC(chunk)
+    print(chunkGC)
+    chunkGCs[i] <- chunkGC
+  }
+  plot(starts,chunkGCs,type="b",xlab="Nucleotide start position",ylab="GC content
+˓→")
+}
+
+
+
