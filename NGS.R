ba51e66ad6ceb046322edc178a8a8c00c9e66581
Dhiraj <dhiraj.jamia@gmail.com>
2019-01-01 16:00:05 +0530

    Update NGS.R

--- NGS.R
+++ NGS.R
@@ -5,3 +5,17 @@ View(df)
 sread(reverseComplement(rfq))
 quality(reverseComplement(rfq))
 quality(trimTails(rfq, 2, "H", successive=TRUE))
+showMethods(class="AlignedRead", where=getNamespace("ShortRead"))
+dirPath <- system.file('extdata', 'maq', package='ShortRead')
+(aln <- readAligned(dirPath, 'out.aln.1.txt', type="MAQMapview"))
+coverage(aln)[[1]]
+cvg <- coverage(aln, shift=c(ChrA=10L))
+## remove 0 coverage on left ends
+ltrim0 <- function(x) {
+    i <- !cumprod(runValue(x) == 0)
+    Rle(runValue(x)[i], runLength(x)[i])
+}
+endoapply(cvg, ltrim0)
+## demonstration of show() and detail() methods
+show(aln)
+detail(aln)
